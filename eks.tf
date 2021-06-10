44ff8b0d77960f98fd69c797ce5e8743a55ebfb7
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-06-10 17:41:49 -0500

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -26,6 +26,7 @@ resource "aws_iam_role" "eks_cluster_role" {
     POLICY
 }
 
+#
 resource "aws_iam_role_policy_attachment" "sales-order-system-AmazonEKSClusterPolicy" {
     policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
     role        = aws_iam_role.eks_cluster_role.name
